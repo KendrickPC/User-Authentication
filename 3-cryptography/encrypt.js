cc4beb99e961d90128dad67b725dd1a47cd6c3b1
kennethchang <kenneth.chang@generalassemb.ly>
2023-02-05 08:49:07 +0800

    encryption of public key notes added

--- 3-cryptography/encrypt.js
+++ 3-cryptography/encrypt.js
@@ -1,9 +1,10 @@
 const crypto = require('crypto')
 
+// If we want to protect data (but not the identity), we encrypt with the public key and decrypt with the private key
 function encryptWithPublicKey(publicKey, message) {
-
+  // Creating node Buffer with utf8 formating
   const bufferMessage = Buffer.from(message, 'utf8')
-
+  // Passing Buffer into public key encrypt func provided by node crypto library
   return crypto.publicEncrypt(publicKey, bufferMessage)
 }
 
