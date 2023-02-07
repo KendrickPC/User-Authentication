4b5bf18439f9c49a25aba02cf11d302b932231a7
kennethchang <kenneth.chang@generalassemb.ly>
2023-02-08 03:46:18 +0800

    npm package base64url installed. JWT header, payload, and signature split

--- 4-JWT/issueJWT.js
+++ 4-JWT/issueJWT.js
@@ -1,2 +1,15 @@
+const base64url = require('base64url')
+
 const JWT = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c';
 
+// 1. Converting JWT into something we can actually work with
+const jwtParts = JWT.split('.')
+// console.log(jwtParts);
+
+const headerInBase64UrlFormat = jwtParts[0];
+const payloadInBase64UrlFormat = jwtParts[1];
+const signatureInBase64UrlFormat = jwtParts[2];
+
+console.log("header", headerInBase64UrlFormat);
+console.log("payload", payloadInBase64UrlFormat);
+console.log("signature", signatureInBase64UrlFormat);
