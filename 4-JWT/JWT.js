19407b4134463f5dcc499ce49191865546f3bb1f
kennethchang <kenneth.chang@generalassemb.ly>
2023-02-08 05:33:56 +0800

    Problem with JWT.js file was that the JWT token string was not valid. Bug is fixed

--- 4-JWT/JWT.js
+++ 4-JWT/JWT.js
@@ -42,16 +42,23 @@ console.log(decodedSignature);
   *ISSUANCE:
 */
 const headerObj = {
-  alg: 'RS256',
-  typ: 'JWT'
-};
+  "alg": "RS256",
+  "typ": "JWT"
+}
 
 const payloadObj = {
-  sub: '1234567890',
-  name: 'John Doe',
-  admin: true,
-  iat: 1516239022
-};
+  "sub": "1234567890",
+  "name": "John Doe",
+  "admin": true,
+  "iat": 1516239022
+}
+
+// const payloadObj = {
+//   sub: '1234567890',
+//   name: 'John Doe',
+//   admin: true,
+//   iat: 1516239022
+// };
 
 const headerObjString = JSON.stringify(headerObj);
 const payloadObjString = JSON.stringify(payloadObj);
@@ -88,7 +95,7 @@ console.log("signatureBase64Url::: ", signatureBase64Url);
  * VERIFICATION
  */
 
-const JWT = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c';
+const JWT = 'eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiYWRtaW4iOnRydWUsImlhdCI6MTUxNjIzOTAyMn0.POstGetfAytaZS82wHcjoTyoqhMyxXiWdR7Nn7A29DNSl0EiXLdwJ6xC6AfgZWF1bOsS_TuYI3OG85AmiExREkrS6tDfTQ2B3WXlrr-wp5AokiRbz3_oB4OxG-W9KcEEbDRcZc0nH3L7LzYptiy1PtAylQGxHTWZXtGz4ht0bAecBgmpdgXMguEIcoqPJ1n3pIWk_dUZegpqx0Lka21H6XxUTxiy8OcaarA8zdnPUnV6AmNP3ecFawIFYdvJB_cm-GvpCSbr8G8y_Mllj8f4x9nBH8pQux89_6gUY618iYv7tuPWBFfEbLxtF2pZS6YC1aSfLQxeNe8djT9YjpvRZA';
 
 const jwtParts = JWT.split('.')
 
--- 4-JWT/README.md
+++ 4-JWT/README.md
@@ -7,3 +7,4 @@
 ![JWT Issuance and Verification](../assets/img/JWT-issuance-and-verification.png)
 
 - [x] RS256 Algorithm
+- [x] Make sure to grab the Public and Private keys from the RS256 algorithm, not the default HS256 algorithm!!!
