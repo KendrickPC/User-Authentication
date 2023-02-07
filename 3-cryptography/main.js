2d6c5c267dceabf9f5c2bb28beae30c3ce163b39
kennethchang <kenneth.chang@generalassemb.ly>
2023-02-08 01:35:56 +0800

    attempting to decrypt

--- 3-cryptography/main.js
+++ 3-cryptography/main.js
@@ -7,4 +7,6 @@ const publicKey = fs.readFileSync(__dirname + '/id_rsa_pub.pem', 'utf8');
 const encryptedMessage = encrypt.encryptWithPublicKey(publicKey, 'Super secret message')
 
 // If you try and "crack the code", you will just get biggerish
+// This is what we will transter over any insecure transport layer.
 console.log("Gibberish::: ", encryptedMessage.toString());
+
